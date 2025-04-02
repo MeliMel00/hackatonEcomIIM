6d7dbe5b776851a4fc5eb67a0c5f65c4230dc8f0
Chevallier Melvin <mchevallier@aubay.com>
2025-04-02 15:30:24 +0200

    test deploy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="bg-gray-100 text-gray-600 text-center py-4">
-            <p>© {new Date().getFullYear()} Shop-Logo. Tous droits réservés.</p>
+            <p>© {new Date().getFullYear()} ShopLogo. Tous droits réservés.</p>
         </footer>
     );
 };
